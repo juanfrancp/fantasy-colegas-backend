b4bcf2c8a3727fb4d504fb1c0474200cd38d95ab
Juan Francisco <juanfrancisco.carcelesp@gmail.com>
2025-08-04 11:49:12 +0200

    Commit inicial

---
+++ src/main/java/com/fantasycolegas/fantasy_colegas_backend/FantasyColegasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fantasycolegas.fantasy_colegas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyColegasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyColegasBackendApplication.class, args);
+	}
+
+}
