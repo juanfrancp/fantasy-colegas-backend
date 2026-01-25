ccb74bd3a63fe9ff4d60cd8decffb740387b4f16
Juan Francisco <juanfrancisco.carcelesp@gmail.com>
2026-01-25 13:23:27 +0100

    Cloudinary para fotos

--- src/main/java/com/fantasycolegas/fantasy_colegas_backend/service/FileStorageService.java
+++ src/main/java/com/fantasycolegas/fantasy_colegas_backend/service/FileStorageService.java
@@ -1,69 +1,41 @@
 package com.fantasycolegas.fantasy_colegas_backend.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.UUID;
-import org.springframework.util.FileSystemUtils;
+import java.util.Map;
 
 @Service
 public class FileStorageService {
 
-    private final Path fileStorageLocation;
+    private final Cloudinary cloudinary;
 
-    public FileStorageService(@Value("${file.upload-dir}") String uploadDir) {
-        this.fileStorageLocation = Paths.get(uploadDir).toAbsolutePath().normalize();
-        try {
-            Files.createDirectories(this.fileStorageLocation);
-        } catch (Exception ex) {
-            throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
-        }
+    public FileStorageService(
+            @Value("${cloudinary.cloud-name}") String cloudName,
+            @Value("${cloudinary.api-key}") String apiKey,
+            @Value("${cloudinary.api-secret}") String apiSecret) {
+
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
+                "secure", true
+        ));
     }
 
     public String storeFile(MultipartFile file, String subfolder) {
-        String originalFileName = StringUtils.cleanPath(file.getOriginalFilename());
-        String fileExtension = "";
         try {
-            fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
-        } catch (Exception e) {
-            fileExtension = "";
-        }
-        String newFileName = UUID.randomUUID().toString() + fileExtension;
+            Map uploadResult = cloudinary.uploader().upload(file.getBytes(),
+                    ObjectUtils.asMap("folder", "fantasy-colegas/" + subfolder));
 
-        try {
-            if (newFileName.contains("..")) {
-                throw new RuntimeException("Sorry! Filename contains invalid path sequence " + newFileName);
-            }
+            return (String) uploadResult.get("secure_url");
 
-            Path targetLocation = this.fileStorageLocation.resolve(subfolder).resolve(newFileName);
-            Files.createDirectories(targetLocation.getParent());
-
-            try (InputStream inputStream = file.getInputStream()) {
-                Files.copy(inputStream, targetLocation, StandardCopyOption.REPLACE_EXISTING);
-            }
-
-            return newFileName;
         } catch (IOException ex) {
-            throw new RuntimeException("Could not store file " + newFileName + ". Please try again!", ex);
-        }
-    }
-
-    public void deleteAll() {
-        FileSystemUtils.deleteRecursively(fileStorageLocation.toFile());
-    }
-
-    public void init() {
-        try {
-            Files.createDirectories(this.fileStorageLocation);
-        } catch (IOException e) {
-            throw new RuntimeException("Could not create upload directory!", e);
+            throw new RuntimeException("Error al subir la imagen a Cloudinary", ex);
         }
     }
 }
